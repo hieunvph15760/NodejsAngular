d23b790d1db04484468091c27c84d94e9bccbec2
nguyenvanhieu <hieunvph15760@fpt.edu.vn>
2022-07-05 20:29:18 +0700

    update projec

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use("/api",routerOrder);
 app.use("/api",routerOrderDetails);
 app.use("/api",routerAuth);
 
-mongoose.connect("mongodb+srv://hieuuchiha:ahieu2k2@cluster0.adz6js0.mongodb.net/bookstore")
+mongoose.connect("mongodb+srv://hieuuchiha:ahieu2k2@cluster0.9xml7uj.mongodb.net/bookstore")
     .then(()=> console.log("Ket noi db thanh cong !"))
 
 
